722333741252ff180bbb3f1e3802a5035e335c56
Akshay Babu <vrakshaybabu@gmail.com>
2023-12-16 12:09:57 +0530

    Setup Stage

---
+++ EV_Reserva/EV_Reserva/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for EV_Reserva project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EV_Reserva.settings')
+
+application = get_asgi_application()
